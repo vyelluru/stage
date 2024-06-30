81bd573de38a9fcb827f2099dc70ed95c16b0ba3
Varun <vyelluru@gmail.com>
2024-06-30 13:41:08 -0700

    change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require("dotenv").config();
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 4000; // Adjust as needed
+const port = process.env.PORT || 4000;
 const model = require("./model.js");
 const cors = require('cors');
 
